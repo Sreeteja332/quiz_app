1569993d186c89d5876c9a122f0993bc3afc1ed5
Sreeteja332 <sreeteja332@gmail.com>
2024-12-18 17:38:14 +0530

    till Index page

--- quiz_page/views.py
+++ quiz_page/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import Http404
 
 # Create your views here.
+
+
+def starting_page(request):
+    try:
+        return render(request,"quiz_page/index.html")
+    except:
+        raise Http404()
